d2d149d6b480faa25ce1fe89f8967449c9a932af
Elijah Enuem-Udogu <koppter.kom@gmail.com>
2019-05-20 13:47:56 +0100

    feature(signup): integrate the signup feature into the signup page
    - integrate the signup script into the signup html page
    
    [Finishes #166115603]

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -1,20 +1,31 @@
-fetch('https://stack-o-lite.herokuapp.com/api/v1/auth/signup', {
-    method: 'post',
-    headers: {
-        "Content-type": "application/json"
-    },
-    body: JSON.stringify({
-        firstName: 'Jane',
-        lastName: 'Doe',
-        email: 'janedoe25@gmail.com',
-        password: 'janedoe25',
-        confirmPassword: 'janedoe25'
-    })
-}).then(response => response.json()).then((data) => {
-    if (data.status === 'success') {
-        localStorage.setItem('stack-o-lite-access-token', data.data.token);
-        // add the script to navigate to Dashboard from here
-    } else {
-        // Display data.message as error message
-    }
+const firstNameField = document.getElementById('firstName');
+const lastNameField = document.getElementById('lastName');
+const emailField = document.getElementById('email');
+const passwordField = document.getElementById('password')
+const confirmPasswordField = document.getElementById('password-confirm');
+const signupForm = document.getElementById('signup-form');
+
+signupForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    fetch('https://stack-o-lite.herokuapp.com/api/v1/auth/signup', {
+        method: 'post',
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            firstName: firstNameField.value,
+            lastName: lastNameField.value,
+            email: emailField.value,
+            password: passwordField.value,
+            confirmPassword: confirmPasswordField.value
+        })
+    }).then(response => response.json()).then((data) => {
+        if (data.status === 'success') {
+            localStorage.setItem('stack-o-lite-access-token', data.data.token);
+            // add the script to navigate to Dashboard from here
+        } else {
+            // Display data.message as error message
+            console.log(data.message);
+        }
+    });
 });
