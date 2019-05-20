33be6e1f52e3fecb7f1f8187bda66cd5daebab90
Elijah Enuem-Udogu <koppter.kom@gmail.com>
2019-05-20 14:08:16 +0100

    feature(login): integrate the login feature into the login page
    add the login fetch script in the login html page and use a login form
    submit event listener to trigger it
    
    [Finishes #166115606]

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,17 +1,24 @@
-fetch('https://stack-o-lite.herokuapp.com/api/v1/auth/login', {
-    method: 'post',
-    headers: {
-        "Content-type": "application/json",
-    },
-    body: JSON.stringify({
-        email: 'johndoe25@gmail.com',
-        password: 'johndoe25',
-    }),
-}).then(response => response.json()).then((data) => {
-    if (data.status === 'success') {
-        localStorage.setItem('stack-o-lite-access-token', data.data.token);
-        // Add script to navigate to Dashboard from here
-    } else {
-        // Use data.message as error message
-    }
+const emailField = document.getElementById('email');
+const passwordField = document.getElementById('password');
+const loginForm = document.getElementById('login-form');
+
+loginForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    fetch('https://stack-o-lite.herokuapp.com/api/v1/auth/login', {
+        method: 'post',
+        headers: {
+            "Content-type": "application/json",
+        },
+        body: JSON.stringify({
+            email: emailField.value,
+            password: passwordField.value,
+        }),
+    }).then(response => response.json()).then((data) => {
+        if (data.status === 'success') {
+            localStorage.setItem('stack-o-lite-access-token', data.data.token);
+            // Add script to navigate to Dashboard from here
+        } else {
+            // Use data.message as error message
+        }
+    });
 });
